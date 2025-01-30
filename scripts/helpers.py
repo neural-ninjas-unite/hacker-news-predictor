6b6f308d9049b6dc82473e82605f0d0701daf247
tommyrharper <thomasrobertharper@gmail.com>
2025-01-30 16:47:01 +0000

    feat: add <UNK> token

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -24,6 +24,7 @@ def create_lookup_tables(words: list[str]) -> tuple[dict[str, int], dict[int, st
   vocab = sorted(word_counts, key=lambda k: word_counts.get(k), reverse=True)
   int_to_vocab = {ii+1: word for ii, word in enumerate(vocab)}
   int_to_vocab[0] = '<PAD>'
+  int_to_vocab[len(int_to_vocab)] = '<UNK>'
   vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
   return vocab_to_int, int_to_vocab
 
--- scripts/weights.py
+++ scripts/weights.py
@@ -16,5 +16,14 @@ print(preprocessed_title)
 with open('lookup_tables.json', 'r') as f:
     lookup_tables = json.load(f)
 
+# lookup_tables['words_to_ids']
+print(lookup_tables['words_to_ids']['ask'])
+
+# Get word IDs, using <UNK> for unknown words
+title_ids = [lookup_tables['words_to_ids'].get(word, lookup_tables['words_to_ids']['<UNK>']) for word in preprocessed_title]
+print('title_ids: ', title_ids)
+
+
+
 # word_ids = [weights['words_to_ids'][word] for word in preprocessed_title]
 
