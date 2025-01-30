b89417828f242996672dac8a46fa3e256b658f41
tommyrharper <thomasrobertharper@gmail.com>
2025-01-30 15:25:09 +0000

    refactor: extract preprocess helper

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,2 +1,23 @@
+import collections
+
 def test_help():
-    print('helping')
+  print('helping')
+
+
+def preprocess(text: str) -> list[str]:
+  text = text.lower()
+  text = text.replace('.',  ' <PERIOD> ')
+  text = text.replace(',',  ' <COMMA> ')
+  text = text.replace('"',  ' <QUOTATION_MARK> ')
+  text = text.replace(';',  ' <SEMICOLON> ')
+  text = text.replace('!',  ' <EXCLAMATION_MARK> ')
+  text = text.replace('?',  ' <QUESTION_MARK> ')
+  text = text.replace('(',  ' <LEFT_PAREN> ')
+  text = text.replace(')',  ' <RIGHT_PAREN> ')
+  text = text.replace('--', ' <HYPHENS> ')
+  text = text.replace('?',  ' <QUESTION_MARK> ')
+  text = text.replace(':',  ' <COLON> ')
+  words = text.split()
+  stats = collections.Counter(words)
+  words = [word for word in words if stats[word] > 5]
+  return words
--- scripts/word2vec.py
+++ scripts/word2vec.py
@@ -4,6 +4,7 @@ import more_itertools
 import requests
 import wandb
 import torch
+from helpers import preprocess
 import os
 from dotenv import load_dotenv
 
@@ -35,28 +36,6 @@ if not os.path.exists('text8'):
 
 with open('text8') as f:
     text8: str = f.read()
-
-
-# #
-# #
-# #
-def preprocess(text: str) -> list[str]:
-  text = text.lower()
-  text = text.replace('.',  ' <PERIOD> ')
-  text = text.replace(',',  ' <COMMA> ')
-  text = text.replace('"',  ' <QUOTATION_MARK> ')
-  text = text.replace(';',  ' <SEMICOLON> ')
-  text = text.replace('!',  ' <EXCLAMATION_MARK> ')
-  text = text.replace('?',  ' <QUESTION_MARK> ')
-  text = text.replace('(',  ' <LEFT_PAREN> ')
-  text = text.replace(')',  ' <RIGHT_PAREN> ')
-  text = text.replace('--', ' <HYPHENS> ')
-  text = text.replace('?',  ' <QUESTION_MARK> ')
-  text = text.replace(':',  ' <COLON> ')
-  words = text.split()
-  stats = collections.Counter(words)
-  words = [word for word in words if stats[word] > 5]
-  return words
 
 
 # # STEP 3: Preprocess the text8 dataset
