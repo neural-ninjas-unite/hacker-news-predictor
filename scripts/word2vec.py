dee4252fc36458529175069d55e49a0625c6a832
tommyrharper <thomasrobertharper@gmail.com>
2025-01-30 15:41:12 +0000

    refactor: extract skipgram into file

--- scripts/word2vec.py
+++ scripts/word2vec.py
@@ -4,6 +4,7 @@ import more_itertools
 import requests
 import wandb
 import torch
+from skipgram import SkipGramFoo
 from helpers import preprocess
 import os
 from dotenv import load_dotenv
@@ -81,25 +82,6 @@ print(len(words_to_ids))         # 63,642
 # # STEP 5: Define the SkipGram model
 # #
 # #
-class SkipGramFoo(torch.nn.Module):
-  def __init__(self, voc, emb, _):
-    super().__init__()
-    self.emb = torch.nn.Embedding(num_embeddings=voc, embedding_dim=emb)
-    self.ffw = torch.nn.Linear(in_features=emb, out_features=voc, bias=False)
-    self.sig = torch.nn.Sigmoid()
-
-  def forward(self, inpt, trgs, rand):
-    emb = self.emb(inpt)
-    ctx = self.ffw.weight[trgs]
-    rnd = self.ffw.weight[rand]
-    out = torch.bmm(ctx, emb.unsqueeze(-1)).squeeze()
-    rnd = torch.bmm(rnd, emb.unsqueeze(-1)).squeeze()
-    out = self.sig(out)
-    rnd = self.sig(rnd)
-    pst = -out.log().mean()
-    ngt = -(1 - rnd + 10**(-3)).log().mean()
-    return pst + ngt
-
 
 # # STEP 6: Create the SkipGram model, optimizer and device
 # #
